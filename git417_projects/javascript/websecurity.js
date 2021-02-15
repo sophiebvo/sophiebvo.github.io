89c8778c5c5855d718e054607ce3d51624694057
sophiebvo <73348689+sophiebvo@users.noreply.github.com>
2021-02-15 15:55:12 -0800

    changed to innerHTML because rubric

--- git417_projects/javascript/websecurity.js
+++ git417_projects/javascript/websecurity.js
@@ -9,23 +9,23 @@
 // function which fills in user info in the span tags by getting the element ids
 
 function populateUserInfo() {
-    // prints user's browser name using faster, more secure textContent instead of innerHTML
-    document.getElementById("userBrowser").textContent = navigator.appName;
+    // prints user's browser name
+    document.getElementById("userBrowser").innerHTML = navigator.appName;
 
     // prints user's platform name
-    document.getElementById("userPlatform").textContent = navigator.platform;
+    document.getElementById("userPlatform").innerHTML = navigator.platform;
 
     // prints user's screen resolution using height and width values
-    document.getElementById("userScreenResolution").textContent = screen.width + " x " + screen.height + " (" + screen.width + " pixels wide by " + screen.height + " pixels tall)";
+    document.getElementById("userScreenResolution").innerHTML = screen.width + " x " + screen.height + " (" + screen.width + " pixels wide by " + screen.height + " pixels tall)";
 
     // print's the approximate dimensions of the user's browser from the outer edges
-    document.getElementById("userWindowSize").textContent = window.outerWidth + " x " + window.outerHeight + " (" + window.outerWidth + " pixels wide by " + window.outerHeight + " pixels tall)";
+    document.getElementById("userWindowSize").innerHTML = window.outerWidth + " x " + window.outerHeight + " (" + window.outerWidth + " pixels wide by " + window.outerHeight + " pixels tall)";
 
     // print's user's available size for web content, not counting space for OS menus
-    document.getElementById("userAvailContentSize").textContent = screen.availWidth + " x " + screen.availHeight + " (" + screen.availWidth + " pixels wide by " + screen.availHeight + " pixels tall)";
+    document.getElementById("userAvailContentSize").innerHTML = screen.availWidth + " x " + screen.availHeight + " (" + screen.availWidth + " pixels wide by " + screen.availHeight + " pixels tall)";
     
     // prints user's color depth
-    document.getElementById("userColorDepth").textContent = screen.colorDepth + " bits";
+    document.getElementById("userColorDepth").innerHTML = screen.colorDepth + " bits";
 }
 
 
