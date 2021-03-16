553db044cee3e27f0a58e40d571504123ef37ae9
sophiebvo <73348689+sophiebvo@users.noreply.github.com>
2021-03-16 05:24:08 -0700

    small fixes/code comments/cleanup

--- git417_git418_projects/javascript/custom_object.js
+++ git417_git418_projects/javascript/custom_object.js
@@ -1,13 +1,11 @@
 /*
-    GIT417: Validation Script
+    GIT418: Validation Script
     Author: Sophie Ojcius
     Date: Mar 15, 2021
 */
 
 "use strict";
 
-// global variables
-// var addExercise = new Exercise();
 
 // custom object constructor for exercise info
 
@@ -17,6 +15,9 @@ function Exercise(exerciseType, countType, amount) {
     this.repOrMinAmount = amount;
 }
 
+
+// creates a new Exercise object and stores references to it in var addExercise
+
 function createExercise() {
     var writtenExercise = document.getElementById("exerciseInput").value;
     var repRadioOption = document.getElementById("repRadio");
@@ -24,6 +25,7 @@ function createExercise() {
     var repOrMinOption = "";
     var countAmount = document.getElementById("exerciseAmount").value;
 
+    // writes "repetitions" or "minutes string depending on which radio button was checked, and writes it in plural or singular form depending on how many reps/mins were added to the count
     if (repRadioOption.checked) {
         repOrMinOption ="repetitions";
         
@@ -44,16 +46,8 @@ function createExercise() {
     createNewExercise(addExercise);
 }
 
-function displayTable() {
-    var exerciseTableArea = document.getElementById("workoutPlanDisplay");
 
-    /* Check to see if you even need the if statement!!!!!!
-    if (exerciseTableArea.style.display = "none") {
-        exerciseTableArea.style.display = "block"
-    }*/
-
-    exerciseTableArea.style.display = "block"
-}
+// adds a new row and 2 cells to the table and writes user input/user choice in them
 
 function createNewExercise(exercise) {
     var table = document.getElementById("workoutPlanTable");
@@ -63,11 +57,25 @@ function createNewExercise(exercise) {
     exerciseRow.insertCell().textContent = exercise.repOrMinAmount + " " + exercise.repOrMin;
 }
 
+
+// makes the table visible in case it was invisible before (at the start of making the working of after clearing all non-header rows)
+
+function displayTable() {
+    var exerciseTableArea = document.getElementById("workoutPlanDisplay");
+
+    exerciseTableArea.style.display = "block"
+}
+
+
+// prevents "submission" default behavior, mostly keeps browser validation error messages from appearing right after correctly filled form is submitted
+
 function preventSubmission(event) {
     event.preventDefault();
 }
 
- //ADD THIS INSIDE THE CREATE OBJECT FUNCTION?
+
+// when #submitExercise button is clicked, function determines if form was fully filled out, and if so, calls upon the functions which: prevent default submission behavior, display the div which contains the table, and create a new exercise object + place it in a new row/cells (also resets the form fields to be empty of user input with the reset() method)
+
 function allowExerciseAdditionAndCleanUp() { // change event to submitActions???
     var exerciseForm = document.getElementById("workoutForm");
 
@@ -81,30 +89,27 @@ function allowExerciseAdditionAndCleanUp() { // change event to submitActions???
         preventSubmission(event); // prevents "submission" default behavior, keeps error messages from appearing right after proper submission
         displayTable();
         createExercise();
+
         exerciseForm.reset();
     }
 }
+
+
+// when #removeExercise button is clicked, function will remove the last row that was added with user input, and if all non-table header rows are removed, will hide the entire #workoutPlanDisplay div
 
 function removeLastRow() {
     var exerciseTableArea = document.getElementById("workoutPlanDisplay");
     var table = document.getElementById("workoutPlanTable");
     var numberOfRows = table.rows.length;
 
-    if (numberOfRows > 2) {
-        table.deleteRow(numberOfRows-1);
-    } else {
-        table.deleteRow(numberOfRows-1);
-        exerciseTableArea.style.display = "none"
+    if (numberOfRows > 2) { // if there are more than 2 table rows
+        table.deleteRow(numberOfRows-1); // remove last row (index number is 1 less than number of rows)
+    } else { // otherwise (if less than 2 rows)
+        table.deleteRow(numberOfRows-1); // remove last row
+        exerciseTableArea.style.display = "none" // and make entire div surrounding table/button/line invisible
     }
 }
-////////////////
-function deleterow(tableID) {
-    var table = document.getElementById(tableID);
-    var rowCount = table.rows.length;
 
-    table.deleteRow(rowCount -1);
-}
-///////////////////
 
 // creates event listeners to trigger the functions above
 
